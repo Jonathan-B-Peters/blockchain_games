14bd922c71890def1e061230f96be87e1e229ed8
Jonathan Peters <47485854+Jonathan-B-Peters@users.noreply.github.com>
2022-09-09 22:25:54 -0700

    Update GameManager.sol

--- contracts/GameManager.sol
+++ contracts/GameManager.sol
@@ -6,6 +6,8 @@ import "hardhat/console.sol";
 
 contract GameManager {
 
+    mapping(address => bool) admins;
+
     struct Game {
         address player1;
         address player2;
@@ -13,5 +15,11 @@ contract GameManager {
         uint256 wager;
     }
 
-    constructor() {}
+    constructor(address _admin) {
+        admins[_admin] = true;
+    }
+
+    function CreateGame() {
+        require(admins[msg.sender], "Only approved addresses can create games.");
+    }
 }
